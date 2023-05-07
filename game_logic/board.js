38c352d683a9ff14861894b4024354e938aaaa2b
FreazeMood <hibdhz221@gmail.com>
2023-05-07 19:22:33 +0300

    landing created required files added.

---
+++ game_logic/board.js
@@ -0,0 +1,13 @@
+class Board {
+    
+    constructor({ cell_size, rows, columns }) {
+        this.cell_size = cell_size;
+        this.rows = rows;
+        this.columns = columns;
+    }
+    
+    
+    init() {
+        // initialize the board on the document. 
+    }
+}
---
+++ game_logic/shape.js
@@ -0,0 +1,14 @@
+class Shape {
+
+    constructor(){
+        this.position = '';
+        this.speed = 500;
+
+    }
+
+    rotate(x, y) {
+
+    }
+
+
+}
