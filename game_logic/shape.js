b72b356b935b8fe5fef3e91f70441f84f3b281c8
FreazeMood <hibdhz221@gmail.com>
2023-05-08 21:54:06 +0300

    added method to create the shapes on the canvas

--- game_logic/shape.js
+++ game_logic/shape.js
@@ -1,13 +1,33 @@
 class Shape {
+    constructor(col, row, color) {    
+        this.col = col;
+        this.row = row;
+        this.color = color;
+        this.speed = 1000; // miliseconds
+    }
 
-    constructor(){
-        this.position = '';
-        this.speed = 500;
+    create(cell_size, ctx) {
+        // calculate the position of the cell on the canvas
+        let x = this.col * cell_size,
+        y = this.row * cell_size;
 
+        // Draw the cell
+        ctx.fillStyle = this.color;
+        ctx.fillRect(x, y, cell_size, cell_size);
+        ctx.strokeStyle = 'purple';
+        ctx.strokeRect(x, y, cell_size, cell_size);
     }
 
     rotate(x, y) {
 
+    }   
+
+    move(position) {
+
+    }
+
+    speed_up() {
+        this.speed = 500;
     }
 
 
