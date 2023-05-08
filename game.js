8b31b5c334b114bc3eb5f50590387b6f6d88acf6
FreazeMood <hibdhz221@gmail.com>
2023-05-08 21:50:10 +0300

    created script to start the game

---
+++ game.js
@@ -0,0 +1,10 @@
+const play_btn = document.getElementById('startGame'),
+launcher = document.getElementById('launcher'),
+gameSection = document.getElementById('game');
+
+play_btn.addEventListener('click', () => {
+    launcher.classList.add('hidden'); 
+    gameSection.classList.remove('hidden');
+    let board = new Board(GRID_SETTINGS);
+    board.createNewBoard();
+})
