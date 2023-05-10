5bf75a3381616fe5b13e851b599219b4ebefef29
FreazeMood <hibdhz221@gmail.com>
2023-05-11 00:47:39 +0300

    movement and deletion of previous placement created

--- game_logic/board.js
+++ game_logic/board.js
@@ -9,7 +9,7 @@ class Board {
         this.shapes = [];
         this.drawBoard();
         this.current_tetromino = this.generateTetromino();
-        this.game_loop = setInterval(() => this.updateBoard(), 1000); // save interval id
+        this.game_loop = setInterval(() => this.updateBoard(), 100); // save interval id
     }
     
     createNewBoard() {
@@ -32,8 +32,8 @@ class Board {
         for (let row = 0; row < this.rows; row++) {
             this.shapes.push([]);
             for (let col = 0; col < this.columns; col++) {
-                const shape = new Shape(this.ctx, col, row, 'rgb(17 24 39)');
-                shape.create(this.cell_size);
+                const shape = new Shape(this.ctx, col, row);
+                shape.create_or_update('rgb(17 24 39)', this.cell_size);
                 this.shapes[row].push(shape);
             
             }
@@ -42,25 +42,48 @@ class Board {
     }  
 
     updateBoard() {
-        // Todo:
-            // remove previous placement after replace
-            //make last shape appear on the board
+       
+            const current_row = this.current_tetromino.current_row,
+            current_shape = this.current_tetromino.current_shape,
+            start_col = this.current_tetromino.current_col,
+            end_col = start_col + current_shape[0].length - 1;
 
-        const current_row = this.current_tetromino.current_row,
-        current_shape = this.current_tetromino.current_shape;
-        // updates the state of the falling tetromino
-        for (let row = current_row; row < current_shape.length + current_row; row++) {
-            for (let col = 0; col < current_shape.length; col++) {
-            this.board[row][col + 4] = current_shape[row][col];
-            this.shapes[row][col + 4].update('yellow', 40);
+            if (current_row > 0) {
+                for (let row = current_row-1; row < current_shape.length + current_row; row++) {
+                    for (let col = start_col; col <= end_col; col++) {
+                        if (this.board[row][col] !== 0) {
+                            this.board[row][col] = 0;
+                            this.shapes[row][col].create_or_update('rgb(17 24 39)', this.cell_size);
+                        }
+                    }
+                }
             }
-          }
-        console.log(this.current_tetromino);
-        console.log(this.board);
+
+            // updates the state of the falling tetromino
+            for (let row = current_row; row < current_shape.length + current_row; row++) {
+                for (let col = start_col; col <= end_col; col++) {
+                    debugger
+                    if (current_row === this.board.length - (current_shape.length)) {
+                        return this.current_tetromino = this.generateTetromino();
+                    }
+                    this.board[row][col] = current_shape[row - current_row][col - start_col];
+                    current_shape[row - current_row][col - start_col] ? this.shapes[row][col].create_or_update('yellow', this.cell_size) : null;
+                }
+            }
+            
+            this.current_tetromino.current_row += 1;
+            console.log(this.current_tetromino);
+            console.log(this.board);
+            
     }
 
     generateTetromino() {
         const randomIndex = Math.floor(Math.random() * POSSIBLE_TETROMINOS.length);
-        return {current_shape: POSSIBLE_TETROMINOS[randomIndex], current_row: 0};
+        return {current_shape: POSSIBLE_TETROMINOS[randomIndex], current_row: 0, current_col: 4};
+    }
+
+    isPlaced() {
+
+
     }
 }
