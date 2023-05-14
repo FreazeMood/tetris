3413eff9aee0f91129d643f217e8fd1c3acbd2e7
FreazeMood <hibdhz221@gmail.com>
2023-05-14 20:31:59 +0300

    tetromino rotation method added

--- game_logic/board.js
+++ game_logic/board.js
@@ -10,8 +10,10 @@ class Board {
         this.ctx = this.createCanvas(canvas_id);
         this.shapes = []; // stores all the objects of Shape;
         this.drawBoard();
+        this.tetrominos = [];
+        this.current_tetromino_id = 1;
         this.current_tetromino = this.generateTetromino();
-        this.game_loop = setInterval(() => this.updateBoard(), 1000); // save interval id
+        this.game_loop = setInterval(() => this.updateBoard(), 100);
         document.addEventListener('keyup', (e) => {
 
             if (e.key === 'ArrowLeft') {
@@ -66,11 +68,11 @@ class Board {
             this.score += 34;
             this.updateScore();
             this.current_tetromino = this.generateTetromino();
-            // if (!this.isMoveValid(this.current_tetromino.current_row, this.current_tetromino.current_col, this.current_tetromino.current_shape)) {
-            //     clearInterval(this.game_loop);
-            //     console.log(this.board);
-            //     alert('Game over!');
-            // }
+            if (!this.isMoveValid(this.current_tetromino.current_row, this.current_tetromino.current_col, this.current_tetromino.current_shape)) {
+                clearInterval(this.game_loop);
+                console.log(this.board);
+                alert('Game over!');
+            }
         }
     }
     
@@ -80,8 +82,9 @@ class Board {
         const randomTetrominoIdx = Math.floor(Math.random() * POSSIBLE_TETROMINOS.length),
         randomColorIdx = Math.floor(Math.random() * TETROMINO_COLORS.length),
         start_col = 4,
-        start_row = 0;
-        return {current_shape: POSSIBLE_TETROMINOS[randomTetrominoIdx], current_row: start_row, current_col: start_col, color: TETROMINO_COLORS[randomColorIdx]};
+        start_row = 0,
+        tetromino = {id: this.current_tetromino_id, current_shape: POSSIBLE_TETROMINOS[randomTetrominoIdx], current_row: start_row, current_col: start_col, color: TETROMINO_COLORS[randomColorIdx]};
+        return tetromino;
     }
 
     moveLeft() {
@@ -124,7 +127,7 @@ class Board {
                 }
             }
         }
-        return true; // no collision detected
+        return true;
     }
     
     
@@ -136,6 +139,7 @@ class Board {
         if (current_row > 0 && current_row !== this.board.length) {
             for (let row = current_row-1; row < current_shape.length + current_row; row++) {
                 for (let col = start_col; col <= end_col; col++) {
+
                     if (this.board[row][col] !== 0) {
                         this.board[row][col] = 0;
                         this.shapes[row][col].create_or_update('rgb(17, 24, 39)', this.cell_size);
@@ -144,6 +148,7 @@ class Board {
             }
         }
     }
+
     
     drawCurrentTetromino() {
         const {current_row, current_col, current_shape, color} = this.current_tetromino;
@@ -164,17 +169,21 @@ class Board {
     }
 
     freezeTetromino() {
-        const { current_row, current_col, current_shape } = this.current_tetromino;
+        const { current_row, current_col, current_shape, color } = this.current_tetromino;
         const start_col = current_col,
               end_col = start_col + current_shape[0].length - 1;
         for (let row = current_row; row < current_shape.length + current_row; row++) {
             for (let col = start_col; col <= end_col; col++) {
                 if (current_shape[row - current_row][col - start_col]) {
                     this.board[row][col] = current_shape[row - current_row][col - start_col];
-                    this.shapes[row][col].create_or_update(this.current_tetromino.color, this.cell_size);
+                    this.shapes[row][col].create_or_update(color, this.cell_size);
                 }
             }
         }
+        this.checkLineCompleted();
+        this.tetrominos.push(this.current_tetromino)
+        this.current_tetromino_id += 1;
+        console.log(this.tetrominos);
     }
     
 
@@ -183,33 +192,35 @@ class Board {
     }
 
     rotate() {
-        const { current_row, current_col, current_shape } = this.current_tetromino;
-        const rotated_shape = [];
-    
-        // Transpose matrix
-        for (let i = 0; i < current_shape.length; i++) {
-            rotated_shape.push([]);
-            for (let j = 0; j < current_shape[i].length; j++) {
-                debugger
-                // if (!current_shape[j][i]) {
-                //     rotated_shape.push([]);
-                // }
-                rotated_shape[i].push(current_shape[j][i]);
-
-            }
-        }
-        // Reverse rows
-        for (let i = 0; i < rotated_shape.length; i++) {
-            rotated_shape[i].reverse();
-        }
-    
-        // Check if new position is valid
+        const { current_row, current_shape } = this.current_tetromino;
+        const rotated_shape = Array.from({length: current_shape[0].length}, () => Array(current_shape.length).fill(0));
         
+        if (current_row > 0) {
+
+            for (let i = 0; i < current_shape.length; i++) {
+                for (let j = 0; j < current_shape[i].length; j++) {
+                    rotated_shape[j][current_shape.length - 1 - i] = current_shape[i][j];
+                }
+            }
+            
+            for (let i = 0; i < rotated_shape.length; i++) {
+                rotated_shape[i].reverse();
+            }
+            
             this.clearCurrentTetromino();
             this.current_tetromino.current_shape = rotated_shape;
             this.drawCurrentTetromino();
-        
-    }
-    
+        }
+      }
+      
+    //   checkLineCompleted() {
+    //     this.board.map((row, idx) => {
+    //         if (row.every(e => e === 1)) {
+    //             this.score += 100  
+    //             this.updateScore();
+    //             this.board[idx].fill(0);
+    //         }    
+    //     })
+    //   }
 
 }
